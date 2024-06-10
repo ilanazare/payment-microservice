274408840a9513ea3bfdcdc894680d5f55e7a49a
ilanazare <ivanazare94@gmail.com>
2024-06-10 14:17:02 -0300

    includes entity for customrs

---
+++ customer/src/main/kotlin/entity/Customers.kt
@@ -0,0 +1,9 @@
+package com.payment.entity
+
+
+data class Customers(
+    private val customerId: String,
+    private val name: String,
+    private val address: String,
+    private val mobileNumber: Number,
+)
