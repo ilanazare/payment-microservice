9a449290bf3a23567fdde7f8f2ad05ce1c417b70
ilanazare <ivanazare94@gmail.com>
2024-08-12 12:20:47 -0300

    docker-compose initial config oracle database

--- customer/src/main/resources/db.migration/V1.1__Customer_Description.sql
+++ customer/src/main/resources/db.migration/V1.1__Customer_Description.sql
@@ -14,4 +14,4 @@ CREATE TABLE customers(
 
 ) ENGINE = InnoDB;
 
-INSERT INTO customers(customer_id, name, address, email, mobile_number, created_at, created_by, updated_at, updated_by) VALUES ('UI2345','iva', 'Brazil', 'iva@mail.com', "71984191099", '01/12/2012' ,'IVA', '02/12/22022', 'IVA');
+--INSERT INTO customers(customer_id, name, address, email, mobile_number, created_at, created_by, updated_at, updated_by) VALUES ('UI2345','iva', 'Brazil', 'iva@mail.com', "71984191099", '01/12/2012' ,'IVA', '02/12/22022', 'IVA');
